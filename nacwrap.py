1bda113cd754a3e2a0e3745979656531dc1d467f
Jordan Maynor <mail@jtm.li>
2024-05-14 16:58:04 -0500

    Initial get instances function

--- nacwrap.py
+++ nacwrap.py
@@ -5,7 +5,7 @@ This module contains functions to interact with the Nintex Workflow Cloud API.
 import json
 import os
 from datetime import datetime
-from typing import Optional
+from typing import Literal, Optional, Union
 
 import requests
 
@@ -124,3 +124,74 @@ def create_instance(workflow_id: str, start_data: Optional[dict] = None) -> dict
         raise Exception(f"Error creating instance for {start_data}")
 
     return response.json()
+
+
+@Decorators.refresh_token
+def get_instance_data(
+    workflow_name: Optional[str] = None,
+    status: Optional[str] = None,
+    order_by: Union[Literal["ASC", "DESC"], None] = None,
+    from_datetime: Optional[datetime] = None,
+    to_datetime: Optional[datetime] = None,
+    page_size: Optional[int] = 100,
+) -> list[dict]:
+    """
+    Get Nintex instance data Follows nextLink until no more pages.
+    Function goes through all instance data in Nintex.
+
+    - workflow_name: Name of the workflow to filter by
+    - status: Status of the workflow to filter by
+    - order_by: Order of the results
+    - from_datetime: Start date to filter by
+    - to_datetime: End date to filter by
+    - page_size: Number of results per page
+
+    Note: If from_datetime and to_datetime are not provided, the Nintex API
+    defaults to returning the last 30 days. If you want everything, you need to
+    explicitly use some sufficiently large time range.
+    """
+    base_url = os.environ["NINTEX_BASE_URL"] + "/workflows/v2/instances"
+    params = {
+        "workflowName": workflow_name,
+        "status": status,
+        "order": order_by,
+        "from": (
+            from_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ") if from_datetime else None
+        ),
+        "to": to_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ") if to_datetime else None,
+        "pageSize": page_size,
+    }
+
+    # Remove None values
+    params = {k: v for k, v in params.items() if v is not None}
+
+    results = []
+    url = base_url
+    first_request = True
+
+    while url:
+        # If this is subsequent requests, don't need to pass params
+        # will be provided in the skip URL
+        if first_request:
+            first_request = False
+        else:
+            params = None
+
+        response = requests.get(
+            url,
+            headers={
+                "Authorization": "Bearer " + os.environ["NTX_BEARER_TOKEN"],
+                "Content-Type": "application/json",
+            },
+            params=params,
+            timeout=30,
+        )
+        pass
+        if response.status_code == 200:
+            data = response.json()
+            results += data["instances"]
+            url = data.get("nextLink")
+        else:
+            print("Error, could not get instance data: ", response.content)
+            break
+    return results
