00ad8959f3f837b7271e86e72274a391599e1c48
Jordan Maynor <mail@jtm.li>
2024-09-27 16:42:24 -0500

    Additional blank files

---
+++ nacwrap/tasks.py
@@ -0,0 +1,11 @@
+"""
+This module contains functions relating to individual task assignments.
+"""
+
+# TODO List tasks
+
+# TODO Get a task
+
+# TODO Complete a task
+
+# TODO Delgate a task assignment
