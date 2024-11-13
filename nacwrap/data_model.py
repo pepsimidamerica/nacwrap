355d5b01b184c73cdcf2d4ce12234aaf2feb3f1f
Chris Lambert <clambert@pepsimidamerica.com>
2024-11-13 16:11:06 -0600

    Added Task Search and User List.

---
+++ nacwrap/data_model.py
@@ -0,0 +1,49 @@
+from enum import Enum
+from datetime import date, datetime
+from typing import Literal, Optional, Union, List
+from pydantic import BaseModel, Field
+
+
+class TaskStatus(Enum):
+    ACTIVE = "active"
+    EXPIRED = "expired"
+    COMPLETE = "complete"
+    OVERRIDDEN = "overridden"
+    TERMINATED = "terminated"
+    ALL = "all"
+
+
+class NintexTask(BaseModel):
+    assignmentBehavior: str
+    completedDate: Optional[datetime] = Field(default=None)
+    completionCriteria: str
+    createdDate: datetime
+    description: str
+    dueDate: datetime
+    id: str
+    initiator: str
+    isAuthenticated: bool
+    message: str
+    modified: datetime
+    name: str
+    outcomes: Optional[List[str]] = Field(default=None)
+    status: TaskStatus
+    subject: str
+    taskAssignments: list
+    workflowId: str
+    workflowInstanceId: str
+    workflowName: str
+
+
+class NintexUser(BaseModel):
+    id: str
+    email: str
+    firstName: str
+    lastName: str
+    isGuest: bool
+    organizationId: str
+    role: str
+
+    @property
+    def name(self):
+        return self.firstName + " " + self.lastName
